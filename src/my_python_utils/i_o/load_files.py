031cc60e427e2aaa0a5deca8c4131002b1035208
sacha-renault <sacha35340@gmail.com>
2024-08-27 23:10:01 +0200

    feat: load_files.batch_file_loader() add a function that aim to load big dataset in batches

--- src/my_python_utils/i_o/load_files.py
+++ src/my_python_utils/i_o/load_files.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Union
+from typing import List, Union, Callable, Any, Generator
 
 def get_all_files(path: str, *, 
                   extension: Union[str, List[str], None] = None, 
@@ -38,3 +38,30 @@ def get_all_files(path: str, *,
         files = [os.path.abspath(file) for file in files]
 
     return files
+
+def batch_file_loader(files: list, 
+                         batch_size: int,
+                         open_file_func: Callable[[str], Any]) -> Generator[List[Any], None, None]:
+    """
+    Generates batches of file contents by loading files in groups of a specified size.
+
+    Args:
+        files (List[str]): A list of file paths to be opened.
+        batch_size (int): The number of files to load in each batch.
+        open_file_func (Callable[[str], Any]): A function that takes a file path as input
+                                               and returns the file's content.
+
+    Yields:
+        Generator[List[Any], None, None]: A generator yielding lists of file contents,
+                                          where each list contains the contents of up to 
+                                          `batch_size` files.
+    """
+    # calculate the number of batch to be opened
+    files_length = len(files)
+    num_batches = (files_length + batch_size - 1) // batch_size
+
+    # generator
+    for i in range(num_batches):
+        start_index = i * batch_size
+        batch = [open_file_func(files[j]) for j in range(start_index, min(start_index + batch_size, files_length))]
+        yield batch
